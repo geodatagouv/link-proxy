17798c632c0e1baa35b44226cd20d728818d1e9d
Bertrand Marron <bertrand.marron@gmail.com>
2017-10-30 14:09:05 +0100

    Add worker jobs structure to link-proxy

---
+++ worker.js
@@ -0,0 +1,17 @@
+const {join} = require('path')
+const delayed = require('delayed-jobs')
+
+const jobsPath = join(__dirname, 'jobs')
+
+delayed.configure({
+  kuePrefix: process.env.KUE_PREFIX || 'q',
+  redisConfig: {
+    host: process.env.REDIS_HOST || 'localhost',
+    port: process.env.REDIS_PORT || 6379
+  },
+  prefix: 'transcode',
+  jobsPath,
+  definitionsPath: `${jobsPath}/definitions.yml`
+})
+
+delayed.startProcessing()
