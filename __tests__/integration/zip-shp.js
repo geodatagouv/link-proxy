c38e92dab7e53eb6bdd13c73c39850c3e76fc039
Bertrand Marron <bertrand.marron@gmail.com>
2018-02-27 16:51:21 +0100

    Create s3 bucket in test

--- __tests__/integration/zip-shp.js
+++ __tests__/integration/zip-shp.js
@@ -2,30 +2,39 @@ const nock = require('nock')
 const {ZipFile} = require('yazl')
 
 const mongo = require('../../lib/mongo')
+const store = require('../../lib/store')
 const analyze = require('../../jobs/check/analyze')
 
+const NAME = 'test-zip-shp'
+
 nock('http://test')
-.get('/data.zip')
-.reply(200, () => {
-  const zip = new ZipFile()
+  .get('/data.zip')
+  .reply(200, () => {
+    const zip = new ZipFile()
 
-  zip.addBuffer(Buffer.from('shp content'), 'data.shp')
-  zip.addBuffer(Buffer.from('shx content'), 'data.shx')
-  zip.addBuffer(Buffer.from('dbf content'), 'data.dbf')
-  zip.addBuffer(Buffer.from('prj content'), 'data.prj')
+    zip.addBuffer(Buffer.from('shp content'), 'data.shp')
+    zip.addBuffer(Buffer.from('shx content'), 'data.shx')
+    zip.addBuffer(Buffer.from('dbf content'), 'data.dbf')
+    zip.addBuffer(Buffer.from('prj content'), 'data.prj')
 
-  zip.end()
+    zip.end()
 
-  return zip.outputStream
-}, {
-  'Transfer-Encoding': 'chunked'
-})
+    return zip.outputStream
+  }, {
+    'Transfer-Encoding': 'chunked'
+  })
 
 beforeAll(async () => {
-  await mongo.connect('mongodb://localhost', 'test-zip-shp')
+  process.env.S3_BUCKET = NAME
+
+  await store.client.createBucket({Bucket: NAME}).promise()
+  await mongo.connect('mongodb://localhost', NAME)
   await mongo.db.dropDatabase()
 })
-afterAll(() => mongo.close())
+afterAll(async () => {
+  await store.client.deleteBucket({Bucket: NAME}).promise()
+  await mongo.close()
+})
 
 describe('zip-shp', () => {
   it('should find a shapefile within the zip file', async () => {
