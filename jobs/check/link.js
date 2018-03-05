c5e385313ac75b19629a923e3756815e931a7bf5
Bertrand Marron <bertrand.marron@gmail.com>
2018-03-05 15:47:35 +0100

    Remove unused method

--- jobs/check/link.js
+++ jobs/check/link.js
@@ -1,55 +1,5 @@
 const mongo = require('../../lib/utils/mongo')
 
-async function checkLink(location) {
-  const now = new Date()
-
-  const {value} = await mongo.db.collection('links').findOneAndUpdate({
-    locations: {
-      $elemMatch: {
-        $eq: location
-      }
-    }
-  }, {
-    $setOnInsert: {
-      createdAt: now
-    },
-    $set: {
-      updatedAt: now
-    },
-    $addToSet: {
-      locations: location
-    }
-  }, {
-    upsert: true,
-    returnOriginal: false
-  })
-
-  const latest = await mongo.db.collection('checks').findOne({
-    location: {
-      $in: value.locations
-    }
-  }, {
-    projection: {
-      number: 1,
-      _id: 0
-    },
-    sort: {
-      number: -1
-    }
-  })
-
-  const {ops} = await mongo.db.collection('checks').insertOne({
-    linkId: value._id,
-    number: latest ? (latest.number || 0) + 1 : 1,
-    createdAt: now,
-    updatedAt: now,
-    state: 'started',
-    location
-  })
-
-  return ops[0]
-}
-
 async function updateLink(check, changes) {
   const remove = changes.filter(c => c.previousDownloadId)
 
@@ -71,4 +21,4 @@ async function updateLink(check, changes) {
   return bulk.execute()
 }
 
-module.exports = {checkLink, updateLink}
+module.exports = {updateLink}
